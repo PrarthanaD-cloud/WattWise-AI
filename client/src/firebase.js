d9db09d0d1b3e15fd79dd9d7594f28b11c39be41
Prarthana D <prarthanadarbhe.com>
2026-05-22 18:35:11 +0530

    Restore firebase config

---
+++ client/src/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp }
+from "firebase/app";
+
+import {
+  getMessaging
+} from "firebase/messaging";
+
+const firebaseConfig = {
+
+  apiKey:
+    "AIzaSyAND34JpxB7R9KlG343jNBM6JwXD2YQnes",
+
+  authDomain:
+    "wattwise-c6ebf.firebaseapp.com",
+
+  projectId:
+    "wattwise-c6ebf",
+
+  storageBucket:
+    "wattwise-c6ebf.firebasestorage.app",
+
+  messagingSenderId:
+    "404446714715",
+
+  appId:
+    "1:404446714715:web:f67b39d4c8b370500f987e",
+
+};
+
+const app =
+  initializeApp(
+    firebaseConfig
+  );
+
+export const messaging =
+  getMessaging(app);
