821e963ffb49f790a9eb6415253c0c5ff8b971d3
Prarthana D <prarthanadarbhe.com>
2026-05-25 07:38:41 +0530

    Fix messaging export

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAND34JpxB7R9KlG343jNBM6JwXD2YQnes",
@@ -13,4 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const messaging = getMessaging(app);
+
+export default app;
